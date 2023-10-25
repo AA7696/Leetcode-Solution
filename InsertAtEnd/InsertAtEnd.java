f5e0feed7691e9230d0d6479df05fbf3e89908f8
Aachman <achmanagarwal7696@gmail.com>
2023-10-26 00:36:53 +0530

    Insert At The End

--- InsertAtEnd/InsertAtEnd.java
+++ InsertAtEnd/InsertAtEnd.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
 
 public class InsertAtEnd {
     public int[] insert(int[] arr, int elem) {
+        int newarr[] = new int[arr.length+1];
+        for (int i=0;i<arr.length;i++){
+            newarr[i] = arr[i];
 
-        
-        return arr;
+        }
+        newarr[newarr.length - 1]=elem;
+       return newarr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,3,4};
+        int elem = 5;
+        InsertAtEnd obj = new InsertAtEnd();
+
+        int[] result = obj.insert(arr, elem);
+
+        System.out.println(Arrays.toString(result));
     }
     
 }
